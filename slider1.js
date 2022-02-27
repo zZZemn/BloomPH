69d56307465bb15598b4f52f0423d7a4a4e6e574
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-02-27 13:31:17 +0800

    file changes

--- slider1.js
+++ slider1.js
@@ -6,25 +6,25 @@ var images3 = [];
 var time = 2000;	
 	 
 
-images[0] = "windBreaker1.jpeg";
-images[1] = "windBreaker2.jpeg";
-images[2] = "windBreaker3.jpeg";
-images[3] = "windBreaker4.jpeg";
+images[0] = "windbreakers/windBreaker1.jpeg";
+images[1] = "windbreakers/windBreaker2.jpeg";
+images[2] = "windbreakers/windBreaker3.jpeg";
+images[3] = "windbreakers/windBreaker4.jpeg";
 
-images1[0] = "windBreaker3.jpeg";
-images1[1] = "windBreaker4.jpeg";
-images1[2] = "windBreaker1.jpeg";
-images1[3] = "windBreaker2.jpeg";
+images1[0] = "windbreakers/windBreaker3.jpeg";
+images1[1] = "windbreakers/windBreaker4.jpeg";
+images1[2] = "windbreakers/windBreaker1.jpeg";
+images1[3] = "windbreakers/windBreaker2.jpeg";
 
-images2[0] = "windBreaker2.jpeg";
-images2[1] = "windBreaker3.jpeg";
-images2[2] = "windBreaker4.jpeg";
-images2[3] = "windBreaker1.jpeg";
+images2[0] = "windbreakers/windBreaker2.jpeg";
+images2[1] = "windbreakers/windBreaker3.jpeg";
+images2[2] = "windbreakers/windBreaker4.jpeg";
+images2[3] = "windbreakers/windBreaker1.jpeg";
 
-images3[0] = "windBreaker4.jpeg";
-images3[1] = "windBreaker1.jpeg";
-images3[2] = "windBreaker2.jpeg";
-images3[3] = "windBreaker3.jpeg";
+images3[0] = "windbreakers/windBreaker4.jpeg";
+images3[1] = "windbreakers/windBreaker1.jpeg";
+images3[2] = "windbreakers/windBreaker2.jpeg";
+images3[3] = "windbreakers/windBreaker3.jpeg";
 
 function changeImg(){
 	document.slide1.src = images[i];
